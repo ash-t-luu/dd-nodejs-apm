77203adc2b8d60035689bed2c0762e03c6ca7989
ash-t-luu <ashley.luu@datadoghq.com>
2024-10-11 12:30:00 -0600

    added new code to test connection

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const tracer = require('dd-trace').init({
     profiling: true,
     env: 'prod',
-    service: 'my-web-app',
-    version: '1.0.3'
+    service: 'dd-nodejs-apm',
+    version: '1.0.0',
+    logInjection: true,
+    debug: true,
+    hostname: 'localhost',
+    port: 8126
 });
 const express = require('express');
-
 const app = express();
 
+const quotes = [
+  "Strive not to be a success, but rather to be of value. - Albert Einstein",
+  "Believe you can and you're halfway there. - Theodore Roosevelt",
+  "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
+];
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const span = tracer.startSpan('get_quote');
+
+  try {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    span.setTag('quote', randomQuote);
+    res.send(randomQuote + '\n'); 
+  } catch (error) {
+    span.setTag('error', true);
+    span.setTag('error.message', error.message);
+    res.status(500).send('An error occurred in generating quote');
+  }
 });
 
 app.listen(3000, () => {
